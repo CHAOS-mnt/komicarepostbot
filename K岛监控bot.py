27b562a05357f8354fadca4818e54de0512256cc
Chaos <114514@yjsnpi.monster>
2020-07-25 18:06:04 +0800

    support embed tag

--- K岛监控bot.py
+++ K岛监控bot.py
@@ -41,44 +41,46 @@ class posts:
         with open('img/'+self.file_name, 'wb') as img:
             img.write(self.d.content)
 
+    def getEmbedLink(self):
+        self.embedlink = htmlTagsToText(str(self.post["embed"]))
+        return self.embedlink
+
 
 def htmlTagsToText(html_string):
     soup=BeautifulSoup(html_string, features="html.parser")
     refer = soup.find_all('span')
     link = soup.find_all('a')
+    div_tag = soup.find_all('div')
+    if len(div_tag) != 0:
+        for i in range(0, len(div_tag)):
+            div = div_tag[i]
+            div_a_link = div.find('a')['href']
+            div_tag[i].replace_with(div_a_link)
+            html_string = str(soup)
+    #处理引用回复
     if len(refer) != 0:
         for m in range(0, len(refer)):
             refer[m].replace_with(refer[m].get_text())
             html_string = str(soup)
+    #处理a标签
     if len(link) != 0:
         for n in range(0, len(link)):
             if str(link[n]).find('onclick') != -1:
                 #print('It/'s Reply')
-                Reply_Target = link[n].get_text()
-                link[n].replace_with(Reply_Target)
+                reply_target = link[n].get_text()
+                link[n].replace_with(reply_target)
                 html_string = str(soup)
             else:
-                #print('It/'s Link')
-                #a标签转MD语法
-                #Url_Text = link[n].get_text()
-                Url_Source = link[n]['href']
-                #Markdown_Link = '[' + Url_Text + ']' + '(' + Url_Source + ')'
-                link[n].replace_with(Url_Source)
+                url_source = link[n]['href']
+                link[n].replace_with(url_source)
                 html_string = str(soup)
-        html_string = html_string.replace("&gt;", ">")
-        html_string = html_string.replace("&amp;", "&")
-        html_string = '#' + str(p.post["no"]) + '\n' + html_string
-        return html_string.replace("<br/>", "\n")
-    else:
-        #print('No Link')
-        html_string = html_string.replace("&gt;", ">")
-        html_string = html_string.replace("&amp;", "&")
-        html_string = '#' + str(p.post["no"]) + '\n' + html_string
-        return html_string.replace("<br/>", "\n")
+    html_string = html_string.replace("&gt;", ">")
+    html_string = html_string.replace("&amp;", "&")
+    return html_string.replace("<br/>", "\n")
 
 def sendImage():
     if 'com' in p.post:
-        text = htmlTagsToText(str(p.post["com"]))
+        text = '#' + str(p.post["no"]) + '\n' + htmlTagsToText(str(p.post["com"]))
     else:
         text = '#' + str(p.post["no"])
     if p.post["ext"] == '.jpg' or p.post["ext"] == '.png' or p.post["ext"] == '.jpeg':
@@ -118,23 +120,32 @@ try:
                 #分别处理不同类型的回复
                 if 'com' in p.post:
                     if 'ext' in p.post:
-                        logging.info('Text with Image')
-                        print(p.post["no"])
+                        logging.info('Text with Image - NO.' + str(p.post["no"]))
                         p.getImage()
-                        text = p.getPostText()
+                        text = '#' + str(p.post["no"]) + '\n' + p.getPostText()#无意义
                         sendImage()
                         last_post_no = p.post["no"]
-                    else:
-                        logging.info('Text Only')
-                        print(p.post["no"])
+                    elif 'embed' in p.post:
+                        logging.info('Text with Youtube Embed - NO.' + str(p.post["no"]))
                         text = p.getPostText()
+                        link = p.getEmbedLink()
+                        text = '#' + str(p.post["no"]) + '\n' + link + '\n' + text
+                        bot.send_message(chat_id=channel_id, text=text)
+                        last_post_no = p.post["no"]
+                    else:
+                        logging.info('Text Only - NO.' + str(p.post["no"]))
+                        text = '#' + str(p.post["no"]) + '\n' + p.getPostText()
                         bot.send_message(chat_id=channel_id, text=text)
                         last_post_no = p.post["no"]
                 elif 'ext' in p.post:
-                    logging.info('Image Only')
-                    print(p.post["no"])
+                    logging.info('Image Only - NO.' + str(p.post["no"]))
                     p.getImage()
                     sendImage()
+                    last_post_no = p.post["no"]
+                elif 'embed' in p.post:
+                    logging.info('Embed Only - NO.' + str(p.post['no']))
+                    text = '#' + str(p.post["no"]) + '\n' + p.getEmbedLink()
+                    bot.send_message(chat_id=channel_id, text=text)
                     last_post_no = p.post["no"]
             else:
                 logging.info('Nothing New')
